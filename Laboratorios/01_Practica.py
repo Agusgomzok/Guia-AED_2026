77b52efd08dcf261f2226ba67a796bb4f318a491
Agustin <gomezagustinivaon07@gmail.com>
2026-06-11 19:52:37 -0300

    11/06

---
+++ Laboratorios/01_Practica.py
@@ -0,0 +1,37 @@
+"""print ("\n")
+print("\t")
+print ("\t hola, \n mundo")
+iva = 0.21
+print (iva)
+x = 1000
+entero = 10
+real = 1.4
+cadena_caracter = "1", "2"
+print (cadena_caracter)
+nombre = "nadin"
+Es_Mayor = True
+print("--------------------------------------------------------------------------------------------------")
+a =0
+b =0
+c =0
+c = int(input("ingrese valor primer valor a operar \n"))
+b = int(input("ingrese segundo valor a operar \n"))
+print("el numero es",c + b)"""
+
+"""print("----------------------------------------------------------------------------")
+numero = int(input("Ingrese un numero para tratarlo\n"))
+
+if numero > :
+    print("numero es mayor a cero")
+else: 
+    print("no lo es")
+print("---------------------------------------------------------------------------------------------------")"""
+print("Simulador de velocidades")
+vel_actual = float(input("Ingrese velocidad actual del movil \n"))
+
+if vel_actual > 120:
+    print("¡Alerta! Exceso de velocidad. Multa en camino.")
+elif vel_actual <= 120 and vel_actual > 40:
+    print("Velocidad adecuada. Buen viaje.")
+else:
+    print("Vehículo demasiado lento. Peligro en autopista.")
